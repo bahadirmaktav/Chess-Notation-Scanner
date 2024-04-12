5a8af44dfe8f6984ea73e108c9040224874f4667
bahadirmaktav <bahadirmaktavv@gmail.com>
2024-04-12 23:34:59 +0300

    Implement template of chnoai module.

---
+++ chnoai/src/chnoai.py
@@ -0,0 +1,24 @@
+class Chnoai:
+    def __init__():
+        None
+
+    # Converts picture of chess notation paper to chess algebraic notation matrix
+    def img2alno(self, image):
+        None
+
+    # Extracts images for each moves
+    def __extract_move_images(self, image):
+        None
+
+    # Extracts symbol images from move images
+    def __extract_symbols(self, move_image):
+        None
+
+    # Uses cnn model to predict symbol and returns the predicted symbol list as characters
+    def __predict_symbols(self, symbol_images):
+        None
+
+    # Maps prediction value to character
+    def __map_symbol_character(self, prediction_value):
+        None
+    
